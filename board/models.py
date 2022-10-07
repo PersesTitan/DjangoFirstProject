43617d4093f44d4c977e297e19526af6b3747ebc
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-10-07 16:38:57 +0900

    update board models

--- board/models.py
+++ board/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+from users.models import User
+
 
 # Create your models here.
 class Board(models.Model):
     title = models.TextField(blank=False)
     content = models.TextField(blank=False)
-    
+    good = models.IntegerField(default=0)   # 좋아요
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, models.CASCADE)
