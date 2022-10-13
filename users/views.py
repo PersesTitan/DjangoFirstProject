71e5efb66d78e5400e928c32e8fa033fb824d3ab
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-10-13 12:18:55 +0900

    add User get_user

--- users/views.py
+++ users/views.py
@@ -13,6 +13,10 @@ from users.models import User
 ID_REPOSITORY = dict()
 
 
+def get_user(request):
+    return ID_REPOSITORY.get(request.COOKIES.get('id'))
+
+
 def set_id(key, value):
     if len(ID_REPOSITORY) != 0 and value in list(ID_REPOSITORY.values()):
         # 존재하는 값 일때 삭제
@@ -32,23 +36,30 @@ def check_blank(li: str, kind: str, request):
 # @login_required
 class LoginUser(View):  # users/login/    login
     def get(self, request):
-        return render(request, 'login.html')
+        user = get_user(request)
+        content = {'user_check': user is not None, 'user': user}
+        return render(request, 'login.html', content)
 
     def post(self, request):
-        data = request.POST
-        username, password = data["id"], data["password"]
-        user = User.objects.filter(Q(username=username) & Q(password=password))
-        if len(user) == 0:
-            messages.error(request, '아이디 또는 비밀번호가 일치하지 않습니다.')
-            return redirect(request.META['HTTP_REFERER'])
-        else:
-            UUID = str(uuid.uuid4())
-            set_id(UUID, user[0])
-
-            url = request.GET.get('next')
-            response = redirect('boards' if (url is None or not url) else url)
-            response.set_cookie('id', UUID)
+        if request.POST.get('logout') is not None:
+            response = redirect(request.META['HTTP_REFERER'])
+            response.delete_cookie('id')
             return response
+        else:
+            data = request.POST
+            username, password = data["id"], data["password"]
+            user = User.objects.filter(Q(username=username) & Q(password=password))
+            if len(user) == 0:
+                messages.error(request, '아이디 또는 비밀번호가 일치하지 않습니다.')
+                return redirect(request.META['HTTP_REFERER'])
+            else:
+                UUID = str(uuid.uuid4())
+                set_id(UUID, user[0])
+
+                url = request.GET.get('next')
+                response = redirect('boards' if (url is None or not url) else url)
+                response.set_cookie('id', UUID)
+                return response
 
 
 @method_decorator(csrf_exempt, name='dispatch')
